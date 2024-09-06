60d0ec1aa683cead586215e10b2ebdd2db09e58d
Philip Swart <philip.swart@chillisoft.co.za>
2024-09-06 16:54:26 +0200

    added FileController

---
+++ src/ChilliStorage.HttpApi.Host/Controllers/FileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ChilliStorage.Interfaces;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace ChilliStorage.Controllers
+{
+    public class FileController : AbpController
+    {
+        private readonly IDocumentConsignmentService _documentConsignmentService;
+
+        public FileController(IDocumentConsignmentService documentConsignmentService)
+        {
+            _documentConsignmentService = documentConsignmentService;
+        }
+
+        [HttpGet]
+        [Route("download/{consignmentNumber}")]
+        public async Task<IActionResult> DownloadAsync(string consignmentNumber)
+        {
+            var fileDto = await _documentConsignmentService.GetDownloadConsignmentDocumentAsync(consignmentNumber);
+
+            return File(fileDto, "application/octet-stream", "file.pdf");
+        }
+    }
+}
